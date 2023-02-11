303006adea0c0a9bda72f0004705a754d4c66471
defmys <defmys@gmail.com>
2023-02-11 18:31:08 +0800

    Added basic codes.

--- BaseCode/main.cpp
+++ BaseCode/main.cpp
@@ -1,4 +1,38 @@
+#include <vulkan/vulkan.h>
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+
+class HelloTriangleApplication
+{
+public:
+    void run()
+    {
+        initVulkan();
+        mainLoop();
+        cleanUp();
+    }
+
+private:
+    void initVulkan() {}
+    void mainLoop() {}
+    void cleanUp() {}
+};
+
+
 int main()
 {
-    return 0;
+    HelloTriangleApplication app;
+    try
+    {
+        app.run();
+    }
+    catch(const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+    
+    return EXIT_SUCCESS;
 }
