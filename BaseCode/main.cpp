b65bff67accfa0eb13faeaa382c818951259af9c
defmys <defmys@gmail.com>
2023-02-11 18:46:20 +0800

    Initialize GLFWwindow

--- BaseCode/main.cpp
+++ BaseCode/main.cpp
@@ -1,4 +1,6 @@
-#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
 #include <iostream>
 #include <stdexcept>
 #include <cstdlib>
@@ -15,9 +17,46 @@ public:
     }
 
 private:
-    void initVulkan() {}
-    void mainLoop() {}
-    void cleanUp() {}
+    void initVulkan() 
+    {
+        if (m_initialized)
+        {
+            return;
+        }
+
+        glfwInit();
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        m_pWindow = glfwCreateWindow(WIDTH, HEIGHT, "Vulkan", nullptr, nullptr);
+
+        m_initialized = true;
+    }
+
+    void mainLoop() 
+    {
+        while(m_pWindow && !glfwWindowShouldClose(m_pWindow))
+        {
+            glfwPollEvents();
+        }
+    }
+
+    void cleanUp() 
+    {
+        if (m_pWindow)
+        {
+            glfwDestroyWindow(m_pWindow);
+            m_pWindow = nullptr;
+        }
+        glfwTerminate();
+    }
+    
+private:
+    const uint32_t WIDTH = 800;
+    const uint32_t HEIGHT = 600;
+
+    GLFWwindow* m_pWindow; 
+    bool m_initialized = false;
 };
 
 
